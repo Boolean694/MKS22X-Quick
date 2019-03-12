cad22556d2835c769cb992074f1b23cc70a872fe
Julian Wong <jwong01@stuy.edu>
2019-03-12 14:48:01 -0400

    got partitn to work

--- Quick.java
+++ Quick.java
@@ -5,7 +5,7 @@ public class Quick {
     int pvind = random.nextInt(en - st) + st;
     int pv = dat[pvind];
     for(int q = 0; q < dat.length; q++) {
-      if(dat[q] > pv && q < pvind) {
+      if(dat[q] >= pv && q < pvind) {
         int tesw = dat[q];
         dat[q] = dat[pvind - 1];
         dat[pvind - 1] = tesw;
@@ -21,14 +21,31 @@ public class Quick {
         dat[pvind + 1] = pv;
         pvind++;
       }
-      else {
-
-      }
+      else {}
     }
-    return 1;
+    for(int q = 0; q < dat.length; q++) {
+      if(dat[q] >= pv && q < pvind) {
+        int tesw = dat[q];
+        dat[q] = dat[pvind - 1];
+        dat[pvind - 1] = tesw;
+        dat[pvind] = tesw;
+        dat[pvind - 1] = pv;
+        pvind--;
+      }
+      else if(dat[q] < pv && q > pvind) {
+        int tesw = dat[q];
+        dat[q] = dat[pvind + 1];
+        dat[pvind + 1] = tesw;
+        dat[pvind] = tesw;
+        dat[pvind + 1] = pv;
+        pvind++;
+      }
+      else {}
+    }
+    return pvind;
   }
   public static void main(String[] fsdf) {
-    int[] da = new int[3];
-    Quick.partition(da, 5, 9);
+    int[] da = {8,4,6,5,2,7,3};
+    Quick.partition(da, 2, 6);
   }
 }
